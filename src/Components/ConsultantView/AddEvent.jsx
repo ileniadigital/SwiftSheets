2c58d0fd0e16e53a9f2cf10c7c9113430529cde1
Farida Addo <f.addo@se22.qmul.ac.uk>
2024-03-13 17:32:42 +0000

    rough draft of add event interface

--- src/Components/ConsultantView/AddEvent.jsx
+++ src/Components/ConsultantView/AddEvent.jsx
@@ -1,13 +1,37 @@
 export default function AddEvent({addEventHandler}) {
 
     return(
-        <div id='addEvent'>
+        <form action="" id='addEvent'>
             <button className = "closeEvent" onClick={addEventHandler}>X</button>
-            <p>Event Name:</p>
-            <p>Date:</p>
-            <p>Event Duration:</p>
-            <p>Event Type:</p>
-            <p>Event Category:</p>
-        </div>
+            <div className="input">
+                <label htmlFor="eventName">Event Name</label>
+                <input type="text" hame = "eventName"/>
+            </div>
+            <div className="input">
+                <label htmlFor="eventDate">Date</label>
+                <input type="text" hame = "eventDate"/>
+            </div>
+            <div className="input">
+                <label htmlFor="eventDuration">Event Duration</label>
+                <input type="text" hame = "eventDuration"/>
+            </div>
+            <div className="input">
+                <label htmlFor="eventType">Event Type</label>
+                <select name="eventType" id="eventType">
+                    <option value="eventTypeNormal">Normal</option>
+                    <option value="eventTypeOvertime">Overtime</option>
+                    <option value="eventTypeHoliday">Holiday</option>
+                    <option value="eventTypeSick">Sick</option>
+                </select>
+            </div>
+            <div className="input">
+                <label htmlFor="eventCategory">Event Category</label>
+                <select name="eventCategory" id="eventCategory">
+                    <option value="Project">Project</option>
+                    <option value="eventCategoryPlanning">Planning</option>
+                    <option value="eventCategoryMeetig">Meeting</option>
+                </select>
+            </div>
+        </form>
     )
 }
