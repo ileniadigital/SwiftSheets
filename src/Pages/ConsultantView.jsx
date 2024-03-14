bfbfc22a61d2e9717ff7c6e103b577abd4cfe91b
Farida Addo <f.addo@se22.qmul.ac.uk>
2024-03-14 11:48:29 +0000

    making week navigation a component

--- src/Pages/ConsultantView.jsx
+++ src/Pages/ConsultantView.jsx
@@ -1,14 +1,13 @@
 // Importing CSS
-import '../CSS/consultantView.css';
+import '../CSS/ConsultantView/ConsultantView.css';
 
 // Importing Icons
-import previousWeekIcon from '../images/consultantView/previousWeekIcon.svg';
-import nextWeekIcon from '../images/consultantView/nextWeekIcon.svg';
 import addEventIcon from '../images/consultantView/addEventIcon.svg';
 import { FaCirclePlus } from "react-icons/fa6";
 
 
 // Importing Components
+import WeekNavigation from '../Components/ConsultantView/WeekNavigation';
 import WeekDay from '../Components/ConsultantView/WeekDay';
 import AddEvent from '../Components/ConsultantView/AddEvent';
 import DailyHours from '../Components/ConsultantView/DailyHours';
@@ -17,54 +16,6 @@ import { useState } from 'react';
 
 export default function ConsultantView() {
 
-    // Function that retrieves the first day of the week (assumed to be monday)
-    function getWeekDay(currentWeek, dayToRetrieve)
-    {
-        let weekDay = new Date(currentWeek); // Creates copy of current week
-
-        /* Subtracts curent day of week to get 'Sunday' (start of the week)
-           Adds daysToRetrrieve to get the target day */
-           weekDay.setDate(weekDay.getDate() - weekDay.getDay() + dayToRetrieve) 
-        
-        /* Series of function calls to return the date in the desired format  
-           .padStart() used to add leading zeroes to the day & month */
-
-        let day = weekDay.getDate().toString().padStart(2, '0');
-        let month = (weekDay.getMonth() + 1).toString().padStart(2,'0'); // + 1 due to 0 indexing
-        let year = weekDay.getFullYear();
-
-        let formattedDate = `${day}/${month}/${year}`
-        return formattedDate 
-    }
-
-    /* Storing the state of the viewed week, allows users to alternate between them */
-    const currentWeek = new Date()
-    const [viewedWeek, setViewedWeek] = useState(new Date());
-
-    /* Function that shows timesheets of the previous week */
-    const changeWeekPrevious = () => {
-        /* Create a temporary variable to store the date a week before*/
-        const newViewedWeek = new Date(viewedWeek);
-        newViewedWeek.setDate(viewedWeek.getDate() - 7)
-        setViewedWeek(newViewedWeek)
-    }
-
-    /* Function that shows timesheets of the next week */
-    const changeWeekNext = () => {
-        // if (currentWeek !== changeWeekNext)
-         /* Create a temporary variable to store the date a week later*/
-         const newViewedWeek = new Date(viewedWeek);
-         newViewedWeek.setDate(viewedWeek.getDate() + 7)
-         setViewedWeek(newViewedWeek)
-    }
-
-    /* Allows users to reset week to current week easily after navigating through diffeent weeks */
-    const resetWeek = () => {setViewedWeek(currentWeek)}
-
-    /* Created to display the week shown */
-    const weekBeginning = getWeekDay(viewedWeek, 1)
-    const weekEnd = getWeekDay(viewedWeek, 7)
-    
     /* Enables relevant screen to be displayed when the + button is clicked */
     const [addEventClicked, setAddEventClicked] = useState(false);
 
@@ -74,24 +25,10 @@ export default function ConsultantView() {
 
     const [submissionStatus, setSubmissionStatus] = useState(false)
 
-
     return(
     <div id = 'consultantView'>
         <div className='consultantViewContainer'>
-            <div className='firstItem'>
-                <div className='weekNavigation'>
-                    <button className='weekIcon'>
-                        <img src={previousWeekIcon} alt="" onClick={changeWeekPrevious}/>
-                    </button>
-                    <div className='currentWeek'>
-                        <h2>{weekBeginning} - {weekEnd}</h2>
-                    </div>
-                    <button className='weekIcon'>
-                        <img src={nextWeekIcon} alt="" onClick={changeWeekNext}/>
-                    </button>
-                </div>
-                <button className = 'resetButton' onClick={resetWeek}>Return to Current Week</button>
-            </div>
+            <WeekNavigation/>
             <button className='addEventButton' onClick={addEventHandler}>
                 <img src={addEventIcon} alt="" />
             </button>
