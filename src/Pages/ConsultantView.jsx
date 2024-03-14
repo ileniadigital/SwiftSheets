44fbc27c9edec38060d1c11c494aa5cc3e1f8635
Farida Addo <f.addo@se22.qmul.ac.uk>
2024-03-14 15:35:35 +0000

    Adding colours based on different timesheet statuses

--- src/Pages/ConsultantView.jsx
+++ src/Pages/ConsultantView.jsx
@@ -6,33 +6,46 @@ import WeekNavigation from '../Components/ConsultantView/WeekNavigation';
 import Week from '../Components/ConsultantView/Week';
 import AddEventButton from '../Components/ConsultantView/AddEventButton';
 
+// Improting state
 import { useState } from 'react';
 
 export default function ConsultantView() {
 
-     /* Enables relevant screen to be displayed when the + button is clicked */
+    // Enables relevant screen to be displayed when the + button is clicked 
      const [addEventClicked, setAddEventClicked] = useState(false);
 
      const addEventHandler = () => {
          setAddEventClicked(!addEventClicked)
      }
 
-    const [submissionStatus, setSubmissionStatus] = useState(false)
+    //  Used to control submission
+    const [timesheetStatus, setTimesheetStatus] = useState("Unsubmitted")
+    const [timesheetReviewStatus, setTimesheetReviewStatus] = useState("Pending")
+    const [timesheetPaymentStatus, setTimesheetPaymentStatus] = useState("Pending")
 
     return(
     <div id = 'consultantView'>
-        <div className='consultantViewContainer'>
+
+        {/* Creating page header */}
+        <div id='consultantViewHeader'>
             <WeekNavigation/>
             <AddEventButton addEventClicked = {addEventClicked} addEventHandler = {addEventHandler}/>
         </div>
-        <div id = 'timesheet'>
-            <Week addEventHandler = {addEventHandler}/>
-        </div>
+
+        {/* Displays currently viewed week, along with hours */}
+        <Week addEventHandler = {addEventHandler}/>
+
+        {/* Used to display the different statuses of the timesheet */}
         <div id='statusContainer'>
-            <p>Status {submissionStatus ? 'Submitted' : 'Unsubmitted'}</p>
-            <p>Review Status</p>
-            <p>Payment Status </p>
-            <button className='submitButton' onClick={() => setSubmissionStatus(true)} disabled={submissionStatus === true}>Submit</button>
+            <div className='centered'>
+                <p>Status <span className={"status " + timesheetStatus.toLowerCase()}>{timesheetStatus}</span></p>
+                <p>Review Status <span className={"status " + timesheetReviewStatus.toLowerCase()}>{timesheetReviewStatus}</span></p>
+                <p>Payment Status <span className={"status " + timesheetPaymentStatus.toLowerCase()}>{timesheetPaymentStatus}</span></p>
+            </div>
+            <div className='buttons'>
+                <button className='submitButton' onClick={() => setTimesheetStatus("Submitted")} disabled = {timesheetStatus === "Submitted"} >{timesheetStatus === "Submitted" ? "Submitted" : "Submit"}</button>
+                <button className='submitButton' onClick={() => setTimesheetStatus("Saved")} disabled = {timesheetStatus === "Submitted"}>Save</button>
+            </div>
         </div>
     </div> 
     )
