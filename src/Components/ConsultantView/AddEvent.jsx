e808980ab2e1b8fa3cd800c6d5a1148adb60e8f4
Farida Addo <f.addo@se22.qmul.ac.uk>
2024-03-15 17:53:57 +0000

    validating date entered

--- src/Components/ConsultantView/AddEvent.jsx
+++ src/Components/ConsultantView/AddEvent.jsx
@@ -10,6 +10,22 @@ import { useState } from 'react';
 export default function AddEvent({componentCaller, addEventHandler, viewedWeek}) {
 
     // eventDuration stored for validation to take place
+    const [eventName, setEventName] = useState('');
+
+    // Ensuring empty string is not entered
+    function validateEventName(event) {
+        const eventName = event.target.value.trim(); 
+        if (eventName.trim() == "") {
+            // setEventName('')
+            event.target.setCustomValidity('Enter an event name');
+        } else {
+            // If value is valid, clear any existing error message
+            // setEventName(eventName);
+            event.target.setCustomValidity('');
+        }
+    }
+
+    // eventDuration stored for validation to take place
     const [eventDuration, setEventDuration] = useState('');
 
     // Ensuring number is a float between 0 (exclusive) and 24
@@ -20,7 +36,7 @@ export default function AddEvent({componentCaller, addEventHandler, viewedWeek})
             event.target.setCustomValidity('Duration must be between 0 (exclusive) and 24 (inclusive)');
         } else {
             // If value is valid, clear any existing error message
-            setEventDuration(duration);
+            // setEventDuration(duration);
             event.target.setCustomValidity('');
         }
     }
@@ -29,10 +45,25 @@ export default function AddEvent({componentCaller, addEventHandler, viewedWeek})
     const {eventDate, setEventDate} = useState('');
 
     function validateEventDate(event) {
-        console.log(viewedWeek)
-    }
+        /* Retrieves start of week (1 added since it starts from 0 as Sunday) */
+        let startOfWeek = new Date(viewedWeek); // Creates copy of current week
+        startOfWeek.setDate(viewedWeek.getDate() - viewedWeek.getDay() + 1) 
 
-    console.log(viewedWeek)
+        // Retrives end of week by adding 6
+        let endOfWeek = new Date(startOfWeek); // Creates copy of current week
+        endOfWeek.setDate(endOfWeek.getDate() + 6)
+        
+        // Converts date entered from string into date, making it easier to compare
+        let eventDate = new Date(event.target.value)
+
+        // Ensures date entered is within week being logged for
+        if (eventDate.getDate() < startOfWeek.getDate() || eventDate.getDate() > endOfWeek.getDate()) {
+            event.target.setCustomValidity('Date must be within the current week')
+        }
+        else {
+            event.target.setCustomValidity('');
+        }
+    }
 
     return(
         <div id='addEvent'>
@@ -42,14 +73,14 @@ export default function AddEvent({componentCaller, addEventHandler, viewedWeek})
             <form action="" id = "addNewEvent">
                 <div className="input">
                     <label htmlFor="eventName">Name</label>
-                    <input type="text" name = "eventName" required/>
+                    <input type="text" name = "eventName" required onChange={validateEventName}/>
                 </div>
 
                 <div className="input">
                     <label htmlFor="eventDate">Date</label>
                 {componentCaller === "Hours" ? (
 
-                    <input type="date" name = "eventDate" value = {viewedWeek} readOnly onChange={validateEventDate} required/>
+                    <input type="date" name = "eventDate" value = {viewedWeek} readOnly required/>
                     ) : (
                     
                     <input type="date" name = "eventDate" onChange={validateEventDate} required/>
@@ -68,7 +99,7 @@ export default function AddEvent({componentCaller, addEventHandler, viewedWeek})
                 </div>
                 <div className="input">
                     <label htmlFor="eventType">Type</label>
-                    <select name="eventType" id="eventType" placeholder='Type' defaultValue={""} required>
+                    <select name="eventType" id="eventType" defaultValue={""} required>
                         <option value="" disabled hidden className='placeHolder'>Type</option> {/* Default value */}
                         <option value="eventTypeNormal">Normal</option>
                         <option value="eventTypeOvertime">Overtime</option>
@@ -85,9 +116,9 @@ export default function AddEvent({componentCaller, addEventHandler, viewedWeek})
                         <option value="eventCategoryMeetig">Meeting</option>
                     </select>
                 </div>
-                <div className="input">
-                    <label htmlFor="isRecurring">Recurring </label>
-                    <input type="checkbox" className='checkBox'/>
+                <div className="input checkBoxContainer">
+                    <label htmlFor="isRecurring" className='checkBoxLabel'>Recurring </label>
+                    <input type="checkbox"/>
                 </div>
                 <input type="submit" value={"Add Event"} className='addEventButton'/>
             </form>
