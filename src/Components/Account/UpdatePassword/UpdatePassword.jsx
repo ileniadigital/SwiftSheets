8245160ebe1393bfa6d9acbe2904afac2939572d
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-03-24 17:22:02 +0000

    Updatepassword front end function

--- src/Components/Account/UpdatePassword/UpdatePassword.jsx
+++ src/Components/Account/UpdatePassword/UpdatePassword.jsx
@@ -2,6 +2,24 @@ import './UpdatePassword.css'; //Import styling
 
 //UpdatePassword Component to update user's password
 export default function UpdatePassword() {
+    let password = 'password'; // FETCH THIS DATA FROM THE DATABASE
+
+    //Function to validate the password
+    function validatePassword(){
+        let currentPassword = document.getElementById('current-password').value;
+        let newPassword = document.getElementById('new-password').value;
+        let confirmPassword = document.getElementById('confirm-password').value;
+        if(currentPassword !== password){
+            alert('Current password is incorrect');
+            return;
+        }
+        if(newPassword !== confirmPassword){
+            alert('New password does not match');
+            return;
+        }
+        alert('Password updated successfully');
+        password=newPassword;
+    }
     return(
         <div className='update-password-container'>
             <h2>Update Password</h2>
@@ -22,7 +40,7 @@ export default function UpdatePassword() {
                     <input type='password' id='confirm-password' name='confirm-password' required/>
                 </div>
                 {/* Submit button */}
-                <button type='submit'>Update Password</button>
+                <button type='submit' onClick={validatePassword}>Update Password</button>
 
             </form>
         </div>
