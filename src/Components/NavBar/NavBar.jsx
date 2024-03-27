481ea5595d6ae3e9c616f26dabc70ec5bf87baca
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-03-27 22:55:30 +0000

    Changed branding and added Language Menu component

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,31 +1,30 @@
 //Import CSS
 import './NavBar.css';
 
-//Import icons
-import { VscAccount } from "react-icons/vsc";
-import { FaHome } from "react-icons/fa";
-import { IoMdSettings } from "react-icons/io";
-
 //Import FDM logo
 import fdmIcon from './logo/fdm-new-logo-green.svg';
 
 //Import Link component
 import Link from './Link';
+import LanguageMenu from './LanguageMenu';
 
 //Navigation bar component
 export default function NavBar(){
     return(
         <div className="navbar-container">
             <nav className="navbar">
+                {/* Logo */}
                 <figure className="navbar-logo">
                     <img src= {fdmIcon} alt='FDM Group Logo'/>
                 </figure>
                 {/* Navigation menu icons */}
                 <ul className="navbar-menu">
-                    <Link href='/Home' className='navbar-link'><FaHome size={40}/></Link>
-                    <Link href='/Account' className='navbar-link'><VscAccount size={40} /></Link>                   
-                    <Link href='/Settings' className='navbar-link'><IoMdSettings size={40} /></Link>
+                    <Link href='/Home' className='navbar-link'>Home</Link>
+                    <Link href='/Account' className='navbar-link'>Account</Link>                   
+                    <Link href='/Settings' className='navbar-link'>Settings</Link>
                 </ul>
+                {/* Language menu */}
+                <LanguageMenu/>
             </nav>
         </div>
     
