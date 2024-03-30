46111d16a4b47a47492d207aee269ee175bbeca5
icetok <istokpetrovic@gmail.com>
2024-03-30 13:18:14 +0000

    caps oops

--- src/Components/Axios.js
+++ src/Components/Axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const baseUrl = 'http://127.0.0.1:8000/'
 const AxiosInstance = axios.create({
-    baseUrl: baseUrl,
+    baseURL: baseUrl,
     timeout: 5000,
     headers: {
         'Content-Type': 'application/json',
