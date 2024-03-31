bccdbb401ee59f4cb715b3a7fc30e3423dfba6bb
Sai Sandeep Vemula <s.s.vemula@se22.qmul.ac.uk>
2024-03-31 15:17:04 +0100

    added some comments

--- backend/myapp/views.py
+++ backend/myapp/views.py
@@ -1,4 +1,5 @@
-# from django.contrib.auth.models import User
+# Import necessary modules and classes
+from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.utils import timezone
@@ -12,22 +13,25 @@ from rest_framework.views import APIView
 from .models import SystemUser, Timesheet, Event, Comment, Notification
 from .serializers import SystemUserSerializer, TimesheetSerializer, EventSerializer, CommentSerializer, NotificationSerializer 
 
+# View to create a new user
 class CreateUserView(generics.CreateAPIView):
-  queryset = SystemUser.objects.all()
-  serializer_class = SystemUserSerializer
-  permission_classes = [AllowAny]
+    queryset = SystemUser.objects.all()
+    serializer_class = SystemUserSerializer
+    permission_classes = [AllowAny]
 
-
+# ViewSet for managing system users
 class SystemUserViewSet(viewsets.ViewSet):
     permission_classes = [permissions.AllowAny]
     queryset = SystemUser.objects.all()
     serializer_class = SystemUserSerializer
 
+    # Retrieve list of all users
     def list(self, request):
         queryset = self.queryset
         serializer = self.serializer_class(queryset, many=True)
         return Response(serializer.data)
 
+    # Create a new user
     def create(self, request):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
@@ -36,11 +40,13 @@ class SystemUserViewSet(viewsets.ViewSet):
         else:
             return Response(serializer.errors, status=400)
 
+    # Retrieve a specific user
     def retrieve(self, request, pk=None):
         system_user = self.queryset.get(pk=pk)
         serializer = self.serializer_class(system_user)
         return Response(serializer.data)
 
+    # Update a user
     def update(self, request, pk=None):
         system_user = self.queryset.get(pk=pk)
         serializer = self.serializer_class(system_user, data=request.data)
@@ -50,39 +56,25 @@ class SystemUserViewSet(viewsets.ViewSet):
         else:
             return Response(serializer.errors, status=400)
 
+    # Delete a user
     def destroy(self, request, pk=None):
         system_user = self.queryset.get(pk=pk)
         system_user.delete()
         return Response(status=204)
     
+# ViewSet for managing timesheets
 class TimesheetViewSet(viewsets.ViewSet):
     permission_classes = [permissions.AllowAny]
     queryset = Timesheet.objects.all()
     serializer_class = TimesheetSerializer
 
+    # Retrieve list of all timesheets
     def list(self, request):
         queryset = self.queryset
         serializer = self.serializer_class(queryset, many=True)
         return Response(serializer.data)
 
-    # def list(self, request):
-    #     user_email = request.query_params.get('user_email')
-    #     print(f"User email: {user_email}")  # Debug print
-
-    #     if user_email is not None:
-    #         user = SystemUser.objects.filter(username=user_email).first()
-    #         print(f"User: {user}")  # Debug print
-
-    #         if user:
-    #             queryset = self.queryset.filter(user=user, end_date__lt=timezone.now().date())
-    #         else:
-    #             queryset = self.queryset.none()  # Handle as appropriate
-    #     else:
-    #         queryset = self.queryset.none()
-
-    #     serializer = self.serializer_class(queryset, many=True)
-    #     return Response(serializer.data)
-
+    # Create a new timesheet
     def create(self, request):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
@@ -91,11 +83,13 @@ class TimesheetViewSet(viewsets.ViewSet):
         else:
             return Response(serializer.errors, status=400)
 
+    # Retrieve a specific timesheet
     def retrieve(self, request, pk=None):
         timesheet = self.queryset.get(pk=pk)
         serializer = self.serializer_class(timesheet)
         return Response(serializer.data)
 
+    # Update a timesheet
     def update(self, request, pk=None):
         timesheet = self.queryset.get(pk=pk)
         serializer = self.serializer_class(timesheet, data=request.data)
@@ -105,21 +99,25 @@ class TimesheetViewSet(viewsets.ViewSet):
         else:
             return Response(serializer.errors, status=400)
 
+    # Delete a timesheet
     def destroy(self, request, pk=None):
         timesheet = self.queryset.get(pk=pk)
         timesheet.delete()
         return Response(status=204)
 
+# ViewSet for managing events
 class EventViewset(viewsets.ViewSet):
     permission_classes = [permissions.AllowAny]
     queryset = Event.objects.all()
     serializer_class = EventSerializer
 
+    # Retrieve list of all events
     def list(self, request):
         queryset = self.queryset
         serializer = self.serializer_class(queryset, many=True)
         return Response(serializer.data)
     
+    # Create a new event
     def create(self, request):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
@@ -128,11 +126,13 @@ class EventViewset(viewsets.ViewSet):
         else:
             return Response(serializer.errors, status=400)
 
+    # Retrieve a specific event
     def retrieve(self, request, pk=None):
         event = self.queryset.get(pk=pk)
         serializer = self.serializer_class(event)
         return Response(serializer.data)
 
+    # Update an event
     def update(self, request, pk=None):
         event = self.queryset.get(pk=pk)
         serializer = self.serializer_class(event, data=request.data)
@@ -142,21 +142,25 @@ class EventViewset(viewsets.ViewSet):
         else:
             return Response(serializer.errors, status=400)
 
+    # Delete an event
     def destroy(self, request, pk=None):
         event = self.queryset.get(pk=pk)
         event.delete()
         return Response(status=204)
     
+# ViewSet for managing comments
 class CommentViewSet(viewsets.ViewSet):
     permission_classes = [permissions.AllowAny]
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
 
+    # Retrieve list of all comments
     def list(self, request):
         queryset = self.queryset
         serializer = self.serializer_class(queryset, many=True)
         return Response(serializer.data)
 
+    # Create a new comment
     def create(self, request):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
@@ -165,11 +169,13 @@ class CommentViewSet(viewsets.ViewSet):
         else:
             return Response(serializer.errors, status=400)
 
+    # Retrieve a specific comment
     def retrieve(self, request, pk=None):
         comment = self.queryset.get(pk=pk)
         serializer = self.serializer_class(comment)
         return Response(serializer.data)
 
+    # Update a comment
     def update(self, request, pk=None):
         comment = self.queryset.get(pk=pk)
         serializer = self.serializer_class(comment, data=request.data)
@@ -179,21 +185,25 @@ class CommentViewSet(viewsets.ViewSet):
         else:
             return Response(serializer.errors, status=400)
 
+    # Delete a comment
     def destroy(self, request, pk=None):
         comment = self.queryset.get(pk=pk)
         comment.delete()
         return Response(status=204)
     
+# ViewSet for managing notifications
 class NotificationViewSet(viewsets.ViewSet):
     permission_classes = [permissions.AllowAny]
     queryset = Notification.objects.all()
     serializer_class = NotificationSerializer
 
+    # Retrieve list of all notifications
     def list(self, request):
         queryset = self.queryset
         serializer = self.serializer_class(queryset, many=True)
         return Response(serializer.data)
 
+    # Create a new notification
     def create(self, request):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
@@ -202,11 +212,13 @@ class NotificationViewSet(viewsets.ViewSet):
         else:
             return Response(serializer.errors, status=400)
 
+    # Retrieve a specific notification
     def retrieve(self, request, pk=None):
         notification = self.queryset.get(pk=pk)
         serializer = self.serializer_class(notification)
         return Response(serializer.data)
 
+    # Update a notification
     def update(self, request, pk=None):
         notification = self.queryset.get(pk=pk)
         serializer = self.serializer_class(notification, data=request.data)
@@ -216,49 +228,30 @@ class NotificationViewSet(viewsets.ViewSet):
         else:
             return Response(serializer.errors, status=400)
 
+    # Delete a notification
     def destroy(self, request, pk=None):
         notification = self.queryset.get(pk=pk)
         notification.delete()
         return Response(status=204)
 
-#a new view to check the events given a timesheet selected
-# class TimesheetEventsView(viewsets.ModelViewSet):
-#     queryset = Timesheet.objects.all()
-#     serializer_class = TimesheetSerializer
-
-#     @action(detail=True, methods=['get'])
-#     def events(self, request, pk=None):
-#         timesheet = self.get_object()
-#         events = Event.objects.filter(timesheet=timesheet)
-#         serializer = EventSerializer(events, many=True)
-#         return Response(serializer.data)
-    
-#     def timesheet_events(request):
-#         timesheets = Timesheet.objects.all()
-#         return render(request, 'timesheet_events.html', {'timesheets': timesheets})
-    
-#     def get_timesheet_events(request, timesheet_id):
-#         timesheet = Timesheet.objects.get(id=timesheet_id)
-#         events = Event.objects.filter(timesheet=timesheet)
-#         event_texts = [event.__str__() for event in events]
-#         return JsonResponse(event_texts, safe=False)
-    
-#select timesheet to view the events for it
+# View to display events related to a selected timesheet
 class TimesheetEventView(APIView):
     renderer_classes = [TemplateHTMLRenderer]
     template_name = 'timesheet_events.html'
 
+    # Display timesheets
     def get(self, request):
         timesheets = Timesheet.objects.all()
         return render(request, 'timesheet_events.html', {'timesheets': timesheets})
 
+    # Display events related to a selected timesheet
     def post(self, request):
         timesheet_id = request.POST.get('timesheet_id')
         timesheet = Timesheet.objects.get(pk=timesheet_id)
         events = timesheet.events.all()  # Retrieve all events related to the selected timesheet
         return render(request, 'timesheet_events.html', {'timesheets': Timesheet.objects.all(), 'timesheet': timesheet, 'events': events})
 
-#select the user to check the timesheets for it.
+# View to display timesheets related to a selected user
 class UserTimesheetView(APIView):
     def get(self, request):
         users = SystemUser.objects.all()
