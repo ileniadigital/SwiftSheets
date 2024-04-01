504a294a7d8f9ca7e857a3bae1f9da7a6fd77b95
Sai Sandeep Vemula <s.s.vemula@se22.qmul.ac.uk>
2024-04-01 16:18:34 +0100

    update

--- src/Components/ConsultantView/PastTimesheets/PastTimesheets.jsx
+++ src/Components/ConsultantView/PastTimesheets/PastTimesheets.jsx
@@ -1,50 +1,50 @@
-// import TimesheetDetails from "../TimesheetDetails/TimesheetDetails";
-
-// export default function PastTimesheets() {
-
-//     const pastTimesheets = JSON.parse(localStorage.getItem('pastTimesheets')) || []
-    
-
-//     let pastTimesheetDetails = []
-//     for (const key in pastTimesheets) {
-//         pastTimesheetDetails.push(
-//         <TimesheetDetails timesheet = {pastTimesheets[key]}/>
-//         )
-//     }
-
-//     return (
-//         <div>
-//             {pastTimesheetDetails}
-//         </div>
-//     )
-// }
-
-import React, { useEffect, useState } from 'react';
-import Axios from 'axios';
 import TimesheetDetails from "../TimesheetDetails/TimesheetDetails";
 
 export default function PastTimesheets() {
-    const [pastTimesheets, setPastTimesheets] = useState([]);
 
-    useEffect(() => {
-        // const userId = localStorage.getItem('userId');
-        const userId = 'Sandeep@gmail.com';
-        Axios.get(`http://localhost:8000/timesheet/?user_email=Sandeep@gmail.com`)
-        .then(response => {
-                setPastTimesheets(response.data);
-            })
-            .catch(error => console.log(error));
-    }, []); 
+    const pastTimesheets = JSON.parse(localStorage.getItem('pastTimesheets')) || []
+    
 
-    console.log(pastTimesheets);
-
-    let pastTimesheetDetails = pastTimesheets.map(timesheet => (
-        <TimesheetDetails key={timesheet.id} timesheet={timesheet}/>
-    ));
+    let pastTimesheetDetails = []
+    for (const key in pastTimesheets) {
+        pastTimesheetDetails.push(
+        <TimesheetDetails timesheet = {pastTimesheets[key]}/>
+        )
+    }
 
     return (
         <div>
             {pastTimesheetDetails}
         </div>
-    );
+    )
 }
+
+// import React, { useEffect, useState } from 'react';
+// import Axios from 'axios';
+// import TimesheetDetails from "../TimesheetDetails/TimesheetDetails";
+
+// export default function PastTimesheets() {
+//     const [pastTimesheets, setPastTimesheets] = useState([]);
+
+//     useEffect(() => {
+//         // const userId = localStorage.getItem('userId');
+//         const userId = 'Sandeep@gmail.com';
+//         Axios.get(`http://localhost:8000/timesheet/?user_email=Sandeep@gmail.com`)
+//         .then(response => {
+//                 setPastTimesheets(response.data);
+//             })
+//             .catch(error => console.log(error));
+//     }, []); 
+
+//     console.log(pastTimesheets);
+
+//     let pastTimesheetDetails = pastTimesheets.map(timesheet => (
+//         <TimesheetDetails key={timesheet.id} timesheet={timesheet}/>
+//     ));
+
+//     return (
+//         <div>
+//             {pastTimesheetDetails}
+//         </div>
+//     );
+// }
