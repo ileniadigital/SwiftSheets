0aafc72ef02758861cff3327043ce66869ecd237
Sandeep <s.s.vemula@se22.qmul.ac.uk>
2024-04-02 01:33:00 +0100

    update

--- src/Components/TimesheetListView/ConsultantTimesheet/ConsultantTimesheet.jsx
+++ src/Components/TimesheetListView/ConsultantTimesheet/ConsultantTimesheet.jsx
@@ -1,45 +1,24 @@
-import "./ConsultantTimesheet.css"; //Import styling
+import './ConsultantTimesheet.css'; //Import styling
 
-import { GrView } from "react-icons/gr";
+import { GrView } from 'react-icons/gr';
 
 //Import components
 import Status from "./Status";
-import Date from "./Date";
-import ConsultantName from "./ConsultantName";
+import Date from './Date';
+import ConsultantName from './ConsultantName';
 
 // Consultant Timesheet component for Line Manager and Finance Team Member
 export default function ConsultantTimesheet(props) {
-  const { name, dates, reviewStatus, paymentStatus, role } = props;
-
-  // Review status changes changes
-  const onReviewStatusChange = (event) => {
-    handleStatusChange(timesheetId, "reviewStatus", event.target.value);
-  };
-
-  // Payment status changes
-  const onPaymentStatusChange = (event) => {
-    handleStatusChange(timesheetId, "paymentStatus", event.target.value);
-  };
-
-  return (
-    <div className="consultantTimesheet-container">
-      <button className="view-icon">
-        <GrView size={30} className="icon" />
-      </button>
-      <ConsultantName name={name} />
-      <Date dates={dates} />
-      {/* Review Status component */}
-      <Status
-        status={reviewStatus}
-        editable={role === "linemanager"}
-        onChange={onReviewStatusChange}
-      />
-      {/* Payment Status component */}
-      <Status
-        status={paymentStatus}
-        editable={role !== "linemanager"}
-        onChange={onPaymentStatusChange}
-      />
-    </div>
-  );
+    const {name, dates, reviewStatus, paymentStatus, role} = props;
+    return (
+        <div className="consultantTimesheet-container">
+            <button className='view-icon'><GrView size={30} className='icon'/></button>
+            <ConsultantName name={name}/>
+            <Date dates={dates}/>
+            {/* Review status */}
+            <Status status={reviewStatus} editable={role==='linemanager'}/>
+            {/* Payment status */}
+            <Status status={paymentStatus} editable={role!=='linemanager'}/>
+        </div>
+    )
 }
