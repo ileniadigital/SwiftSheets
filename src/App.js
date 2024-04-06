77e2ba10019dd7b8ffe60a48c7ac44186eb7857b
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-04-06 23:44:23 +0100

    Styled border of timesheet

--- src/App.js
+++ src/App.js
@@ -10,14 +10,13 @@ import Home from './Pages/Home';
 import Name from './Components/NavBar/Name';
 import SystemAdminView from './Pages/SystemAdminView';
 import ConsultantSettings from './Pages/ConsultantView/ConsultantSettings/ConsultantSettings';
-import TimesheetListView from './Pages/TimesheetListView';
 
 import { useState, useEffect } from 'react';
 import getDate from './Components/ConsultantView/getDate';
 import Reminder from './Components/ConsultantView/Reminder/Reminder';
 
 //ADD ROUTING BASED ON ROLE FROM DB
-const role='systemadmin';
+const role='consultant';
 
 // Main App component
 export default function App() {
