c8c54d5101cf18cd765dbd522f63c1b9ff2b566c
ahzaz tahir <ec22984@qmul.ac.uk>
2024-04-08 19:41:31 +0100

    reset password frontend. please ignore changes to index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 import Login from './Pages/Login';
+import ResetPassword from './Pages/ResetPassword';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Login />
+    <ResetPassword />
   </React.StrictMode>
 );
 
