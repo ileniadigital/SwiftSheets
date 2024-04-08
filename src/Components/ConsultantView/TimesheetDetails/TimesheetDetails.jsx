45a474880beab1ba99d49849711ebc0fc7761a1d
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-04-08 12:57:20 +0100

    changed date format

--- src/Components/ConsultantView/TimesheetDetails/TimesheetDetails.jsx
+++ src/Components/ConsultantView/TimesheetDetails/TimesheetDetails.jsx
@@ -3,13 +3,20 @@ import './TimesheetDetails.css'
 
 /* Displays timesheet's details */
 export default function TimesheetDetails({timesheet}) {
+    function formatDate(dateString) {
+        const date = new Date(dateString);
+        const day = date.getDate().toString().padStart(2, '0');
+        const month = (date.getMonth() + 1).toString().padStart(2, '0');
+        const year = date.getFullYear();
+        return `${day}/${month}/${year}`;
+    }
 
     return (
         <div className="timesheet-details-container">
             <div className='timesheet-details'>
                 <div>
                     <p className='title'>Date</p>
-                    {timesheet.start_date} - {timesheet.end_date}
+                    {formatDate(timesheet.start_date)} - {formatDate(timesheet.end_date)}
                 </div>
                 <div>
                     <p className='title'>Submission Status</p>
