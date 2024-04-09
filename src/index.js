ee6dc067feb7255d79930810568e76cf4023350b
Farida Addo <f.addo@se22.qmul.ac.uk>
2024-04-09 18:41:07 +0100

    commenting to remove error message

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 import Login from './Pages/Login';
-import ResetPassword from './Pages/ResetPassword';
+// import ResetPassword from './Pages/ResetPassword';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ResetPassword />
+    <Login />
   </React.StrictMode>
 );
 
