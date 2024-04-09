373f2a1996d0e8fb00a984430b99e84e139f7359
Farida Addo <f.addo@se22.qmul.ac.uk>
2024-04-09 18:23:20 +0100

    chaging form header, styling

--- src/Components/SystemAdminView/ViewForm/ViewForm.jsx
+++ src/Components/SystemAdminView/ViewForm/ViewForm.jsx
@@ -4,20 +4,26 @@ import './ViewForm.css'
 // Importing Components
 import { IoClose } from "react-icons/io5";
 
-// Importing useState
-import { useState } from 'react';
-
 export default function ViewForm ({ formData, viewFormHandler }) {
 
     return (
         <div id='viewFormMenu'>
             <button className = "closeWindow" onClick={() => viewFormHandler()}><IoClose /></button>
             <div id='viewFormContent'>
-                <h1 className='viewFormHeading'>{formData.type} Form</h1>
+                {/* Add Report to name if it is a bug, otherwise Request */}
+                {
+                    formData.type === 'Bug' ? (
+                        <h1 className='viewFormHeading'>Bug Report</h1>
+
+                    ) :
+                    (
+                        <h1 className='viewFormHeading'>Request</h1>
+                    )
+                }
                 {/* Name text input*/}
                 <div className="input">
                     <label htmlFor="subject" className='label'>Subject</label>
-                    <p id='subject'>{formData.subject}</p>
+                    <p id='subject' className='inputBox'>{formData.subject}</p>
                 </div>
                 <div className='input'>
                     <textarea name="text" id='text' class='inputBox' value={formData.text} disabled />
